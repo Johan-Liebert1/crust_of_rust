3da4c25675460dc5c5794026c43faf84a3f2ea11
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-06-25 18:18:33 +0530

    fix double ended parallel traversal issue

--- src/s3_iterators/iterators.rs
+++ src/s3_iterators/iterators.rs
@@ -1,12 +1,12 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-pub fn flatten<I>(iter: I) -> Flatten<I>
+pub fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
 where
-    I: Iterator,
+    I: IntoIterator,
     I::Item: IntoIterator,
 {
-    Flatten::new(iter)
+    Flatten::new(iter.into_iter())
 }
 
 pub struct Flatten<O>
@@ -15,7 +15,8 @@ where
     O::Item: IntoIterator,
 {
     outer: O,
-    inner: Option<<O::Item as IntoIterator>::IntoIter>,
+    front_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    back_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Flatten<O>
@@ -26,7 +27,8 @@ where
     fn new(iter: O) -> Self {
         Flatten {
             outer: iter,
-            inner: None,
+            front_iter: None,
+            back_iter: None,
         }
     }
 }
@@ -40,22 +42,101 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner_iter) = self.inner {
+            if let Some(ref mut inner_iter) = self.front_iter {
                 if let Some(i) = inner_iter.next() {
                     return Some(i);
                 }
-                // we have exhausted the inner iterator as next didn't give back any item
-                self.inner = None;
+                // we have exhausted the front_iter iterator as next didn't give back any item
+                self.front_iter = None;
             }
 
-            let next_inner_iter = self.outer.next()?.into_iter();
-            self.inner = Some(next_inner_iter);
+            if let Some(next_inner) = self.outer.next() {
+                self.front_iter = Some(next_inner.into_iter())
+            } else {
+                return self.back_iter.as_mut()?.next();
+            }
+        }
+    }
+}
+
+impl<O> DoubleEndedIterator for Flatten<O>
+where
+    O: DoubleEndedIterator, // outer thing implements iterator + DoubleEndedIterator
+    O::Item: IntoIterator,  // items of the outer type implement IntoIterator
+    <O::Item as IntoIterator>::IntoIter: DoubleEndedIterator, // items of the outer type implement DoubleEndedIterator
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(ref mut back_iter) = self.back_iter {
+                if let Some(i) = back_iter.next_back() {
+                    return Some(i);
+                }
+                // we have exhausted the front_iter iterator as next didn't give back any item
+                self.front_iter = None;
+            }
+
+            if let Some(next_inner) = self.outer.next_back() {
+                self.back_iter = Some(next_inner.into_iter())
+            } else {
+                return self.front_iter.as_mut()?.next_back();
+            }
         }
     }
 }
 
 pub fn tests() {
     println!("Testing iterators");
+
+    // empty
+    assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0);
+
+    // empty_wide
+    assert_eq!(flatten(vec![Vec::<()>::new(), vec![], vec![]]).count(), 0);
+
+    // one
+    assert_eq!(flatten(std::iter::once(vec!["a"])).count(), 1);
+
+    // two
+    assert_eq!(flatten(std::iter::once(vec!["a", "b"])).count(), 2);
+
+    // two_wide
+    assert_eq!(flatten(vec![vec!["a"], vec!["b"]]).count(), 2);
+
+    // reverse
+    assert_eq!(
+        flatten(std::iter::once(vec!["a", "b"]))
+            .rev()
+            .collect::<Vec<_>>(),
+        vec!["b", "a"]
+    );
+
+    // reverse_wide
+    assert_eq!(
+        flatten(vec![vec!["a"], vec!["b"]])
+            .rev()
+            .collect::<Vec<_>>(),
+        vec!["b", "a"]
+    );
+
+    // both ends
+    let mut iter = flatten(vec![vec!["a1", "a2", "a3"], vec!["b1", "b2", "b3"]]);
+    assert_eq!(iter.next_back(), Some("b3"));
+    assert_eq!(iter.next(), Some("a1"));
+    assert_eq!(iter.next(), Some("a2"));
+    assert_eq!(iter.next(), Some("a3"));
+    assert_eq!(iter.next_back(), Some("b2"));
+    assert_eq!(iter.next_back(), Some("b1"));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.next_back(), None);
+
+    // infinitely flatten
+    let mut iter = flatten((0..).map(|i| 0..i));
+    // 0 => 0..0 => empty
+    // 1 => 0..1 => [0]
+    // 2 => 0..2 => [0, 1]
+    assert_eq!(iter.next(), Some(0));
+    assert_eq!(iter.next(), Some(0));
+    assert_eq!(iter.next(), Some(1));
 }
 
 fn iterator_examples() {
