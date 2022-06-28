4e8a730ee2b94bda9f29fbec32da9ee5bb52f04b
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-06-28 23:31:33 +0530

    pantom data

--- src/s4_smart_pointers_interior_mutability/rc.rs
+++ src/s4_smart_pointers_interior_mutability/rc.rs
@@ -1,6 +1,38 @@
+#![allow(dead_code)]
+#![allow(unused_must_use)]
+#![allow(unused_variables)]
 // Reference counted pointer
 use super::cell::MyCell;
+use std::marker::PhantomData;
 use std::ptr::NonNull;
+
+// ============================ THE DROP CHECK =========================================
+struct Foo<'a, T: Default> {
+    v: &'a mut T,
+}
+
+impl<T: Default> Drop for Foo<'_, T> {
+    #[allow(unused_must_use)]
+    fn drop(&mut self) {
+        std::mem::replace(self.v, T::default());
+    }
+}
+
+fn bad_things_happen() {
+    // t is dropped before foo as Rust maintains a stack of variables to be dropped
+    let foo: Foo<String>;
+    let mut t;
+    t = String::from("hello");
+    // foo = MyRc::new(Foo { v: &mut t });
+} // here foo is dropped and dropping a struct is counted as dropping every single value of that
+  // struct. So Rust will call the drop implementation of Foo, but in Foo we're accessing the dropped
+  // string t, and the code won't compile
+  //
+  // But, with a MyRef, Rust does not know about the "T" that MyRef contains and if we surround the
+  // declaration of Foo by MyRef, i.e. MyRc::new(Foo{ v: &mut t }), it will compile the code as the
+  // compiler does not know about the t inside of Foo inside of MyRc
+
+// ============================ THE DROP CHECK =========================================
 
 struct RcInner<T> {
     value: T,
@@ -11,6 +43,7 @@ pub struct MyRc<T> {
     // T is stored on the heap. It needs to be on the heap as multiple functions can be pointing to
     // T so T cannot be on the stack frame of any particular function
     inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>, // treat as if MyRc contains T
 }
 
 impl<T> MyRc<T> {
@@ -24,6 +57,7 @@ impl<T> MyRc<T> {
             MyRc {
                 // SAFETY: Box does not give us a null pointer
                 inner: NonNull::new_unchecked(Box::into_raw(inner)),
+                _marker: PhantomData,
             }
         }
     }
@@ -38,7 +72,10 @@ impl<T> Clone for MyRc<T> {
             let c = inner.refcount.get();
             inner.refcount.set(c + 1);
 
-            MyRc { inner: self.inner }
+            MyRc {
+                inner: self.inner,
+                _marker: PhantomData,
+            }
         }
     }
 }
