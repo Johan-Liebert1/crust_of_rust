9352a972518152dbbc8332a58688800643715097
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-07-02 15:00:00 +0530

    check for open senders before receiver taking the lock

--- src/s5_channels/channels.rs
+++ src/s5_channels/channels.rs
@@ -5,44 +5,69 @@ use std::sync::{Arc, Condvar, Mutex}; // VecDeque = kinda like a ring buffer
                                       // Arc = Atomically reference counted type
 
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        // keep track of how many senders there are. We do this to not keep a receiver waiting when
+        // all the senders have been dropped
+        inner.senders += 1;
+        drop(inner);
+
         Self {
-            // to tell Rust to clone the Arc, i.e. the reference counter instead of the inner thing
+            // to tell Rust to clone the Arc, i.e. the reference counter instead of the shared thing
             // that Arc holds which is the VecDeque
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last = inner.senders == 0;
+        drop(inner);
+
+        if was_last {
+            // last sender drop. Wake the receiver
+            self.shared.available.notify_one();
         }
     }
 }
 
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(t);
-        drop(queue);
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(t);
+        drop(inner);
+
         // notify any waiting receiver when it sends
-        self.inner.available.notify_one();
+        self.shared.available.notify_one();
     }
 }
 
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
-    pub fn receive(&mut self) -> T {
-        let mut queue = self.inner.queue.lock().unwrap();
+    pub fn receive(&mut self) -> Option<T> {
+        let mut inner = self.shared.inner.lock().unwrap();
 
         loop {
-            match queue.pop_front() {
-                Some(t) => return t,
+            match inner.queue.pop_front() {
+                Some(t) => return Some(t),
+
+                // if the sender count is 0, then just
+                // return as the channel is empty forever
+                None if inner.senders == 0 => return None,
                 None => {
                     // this loop won't consume CPU cycles as it is put to sleep by the OS
                     // wait gives up the lock
-                    queue = self.inner.available.wait(queue).unwrap();
+                    inner = self.shared.available.wait(inner).unwrap();
                 }
             }
         }
@@ -51,24 +76,35 @@ impl<T> Receiver<T> {
 
 struct Inner<T> {
     // things in the channel
-    queue: Mutex<VecDeque<T>>,
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
+struct Shared<T> {
+    // things in the channel
+    inner: Mutex<Inner<T>>,
     available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default(),
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+
+    let shared = Shared {
+        inner: Mutex::new(inner),
         available: Condvar::new(),
     };
 
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
 
     (
         Sender {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     )
 }
@@ -79,5 +115,16 @@ pub fn tests() {
     // ping pong
     let (mut tx, mut rx) = channel();
     tx.send(42);
-    assert_eq!(rx.receive(), 42);
+    assert_eq!(rx.receive(), Some(42));
+
+    // closed tx
+    let (mut tx, mut rx) = channel::<()>();
+    drop(tx);
+    // let _ = rx.receive(); // what do we even get here? this will just hang forever
+    assert_eq!(rx.receive(), None);
+
+    // closed rx
+    let (mut tx, mut rx) = channel::<()>();
+    drop(rx);
+    tx.send(42); // we should tell that channel has been closed as there are no receivers
 }
