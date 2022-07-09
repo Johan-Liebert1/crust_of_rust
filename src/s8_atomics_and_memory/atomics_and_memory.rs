ba0b616d491861882f92e7c6e18e1dd563ab5475
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-07-09 19:55:30 +0530

    ordering::relaxed

--- src/s8_atomics_and_memory/atomics_and_memory.rs
+++ src/s8_atomics_and_memory/atomics_and_memory.rs
@@ -33,14 +33,19 @@ impl<T> MyMutex<T> {
         ret
     }
 
-    // takes a closure function
-    pub fn with_good_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+    // NOT_TOO_GOOD: Checkout test too_relaxed for more info
+    pub fn with_okayish_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
         while self
             .locked
-            .compare_exchange(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed)
+            .compare_exchange_weak(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed)
             .is_err()
         {
             // MESI protocol
+
+            // as compare_exchange is quite expensive due to the CPU needing an exclusive reference
+            // to the memory location, we will just read the value until it changes to our desired
+            // value. Reading only needs shared access to the memory location
+            while self.locked.load(Ordering::Relaxed) == LOCKED {}
         }
         // maybe another thread runs here...
 
@@ -48,6 +53,30 @@ impl<T> MyMutex<T> {
         let ret = f(unsafe { &mut *self.v.get() });
 
         self.locked.store(UNLOCKED, Ordering::Relaxed);
+
+        ret
+    }
+
+    pub fn with_good_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        while self
+            .locked
+            .compare_exchange_weak(UNLOCKED, LOCKED, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {
+            // MESI protocol
+
+            // as compare_exchange is quite expensive due to the CPU needing an exclusive reference
+            // to the memory location, we will just read the value until it changes to our desired
+            // value. Reading only needs shared access to the memory location
+            while self.locked.load(Ordering::Relaxed) == LOCKED {}
+        }
+        // maybe another thread runs here...
+
+        // SAFETY: We hold the lock therefore we can create a mutable reference
+        let ret = f(unsafe { &mut *self.v.get() });
+
+        // Ordering::Release makes sure to not reorder the statement while executing
+        self.locked.store(UNLOCKED, Ordering::Release);
 
         ret
     }
@@ -78,7 +107,77 @@ pub mod tests {
             handle.join().unwrap();
         }
 
-        assert_eq!(l.with_bad_lock(|v| *v), 100 * 1000);
+        // assert_eq!(l.with_bad_lock(|v| *v), 100 * 1000);
+    }
+
+    #[test]
+    fn too_relaxed() {
+        use std::sync::atomic::AtomicUsize;
+
+        let x: &'static _ = Box::leak(Box::new(AtomicUsize::new(0)));
+        let y: &'static _ = Box::leak(Box::new(AtomicUsize::new(0)));
+
+        let t1 = spawn(move || {
+            let r1 = y.load(Ordering::Relaxed);
+            x.store(r1, Ordering::Relaxed);
+            r1
+        });
+        let t2 = spawn(move || {
+            let r2 = x.load(Ordering::Relaxed);
+
+            // this line might execute before the previous line due to CPU / compiler optimizing things
+            // which is allowed as the following line does not depend on anything that the previous
+            // line does as the following line does not use r2 and neither x
+            // NOTE: Odering::Relaxed allows this to happen
+            y.store(42, Ordering::Relaxed);
+            r2
+        });
+
+        let r1 = t1.join().unwrap();
+        let r2 = t2.join().unwrap();
+
+        // it is possible for the following to happen
+        // r1 == r2 == 42
+    }
+
+    #[test]
+    fn sync_test() {
+        use std::sync::atomic::AtomicUsize;
+
+        let x: &'static _ = Box::leak(Box::new(AtomicBool::new(false)));
+        let y: &'static _ = Box::leak(Box::new(AtomicBool::new(false)));
+        let z: &'static _ = Box::leak(Box::new(AtomicUsize::new(0)));
+
+        spawn(move || {
+            x.store(true, Ordering::Release);
+        });
+
+        spawn(move || {
+            y.store(true, Ordering::Release);
+        });
+
+        let t1 = spawn(move || {
+            while !x.load(Ordering::Acquire) {}
+
+            if y.load(Ordering::Acquire) {
+                z.fetch_add(1, Ordering::Relaxed);
+            }
+        });
+
+        let t2 = spawn(move || {
+            while !y.load(Ordering::Acquire) {}
+
+            if x.load(Ordering::Acquire) {
+                z.fetch_add(1, Ordering::Relaxed);
+            }
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        let z = z.load(Ordering::SeqCst);
+
+        // What are the possible values for z?
     }
 }
 
